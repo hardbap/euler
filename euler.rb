f41dd874ec9cde7f990937cc62bb49839d6f91f0
hardbap <hardbap@gmail.com>
2017-09-13 06:57:57 -0400

    Add a runner

---
+++ euler.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require_relative('./lib/round')
+require 'csv'
+
+player_1_wins = 0
+game_number = 0
+
+CSV.foreach('./resources/poker.txt', col_sep: " ") do |row|
+  game_number += 1
+
+  hands = row.each_slice(5).to_a
+  round = Round.new(hands.first, hands.last)
+  puts "Game #{game_number}: P1 #{hands.first}, P2 #{hands.last}"
+
+  if round.player_1_wins?
+    player_1_wins += 1
+    puts 'P1 wins'
+  end
+end
+
+puts "\n Total wins for Player 1: #{player_1_wins}"
